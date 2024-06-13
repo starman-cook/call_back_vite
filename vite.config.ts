e3d3e78dcbed320c3b6e65c2069aa5d22a4205b6
Pasha Mishakov <pashamishakov@gmail.com>
2024-06-13 21:27:49 +0500

    added jquery to test

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin({
-    preRenderCSSCode: (cssCode) => {
-      console.log(cssCode)
-      return cssCode.replace(/\/assets/g, 'https://callbackvite.netlify.app/assets')
-    }}
-  )],
-  server: {
-    port: 3000
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "index.js",
-        manualChunks: undefined,
-      },
+    plugins: [
+        react(),
+        cssInjectedByJsPlugin({
+            preRenderCSSCode: (cssCode) => {
+                console.log(cssCode);
+                return cssCode.replace(/\/assets/g, 'https://callbackvite.netlify.app/assets');
+            },
+        }),
+    ],
+    server: {
+        port: 3000,
     },
-  },
-})
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'index.js',
+                manualChunks: undefined,
+            },
+        },
+    },
+});
