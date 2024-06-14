a1a20993153086c853cad926dbdb085262f08df4
Pasha Mishakov <pashamishakov@gmail.com>
2024-06-14 09:40:36 +0500

    removed creation of root div

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,4 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-const div: HTMLDivElement = document.createElement('div');
-document.body.appendChild(div);
-
-ReactDOM.createRoot(div!).render(<App />);
+ReactDOM.createRoot(document.getElementById('call_back_vite')!).render(<App />);
