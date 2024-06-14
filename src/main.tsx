481a319505119bfb3c7fc9d8b96a92f40438c3a6
Pasha Mishakov <pashamishakov@gmail.com>
2024-06-14 09:53:43 +0500

    add MutationObserver

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,24 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('call_back_vite')!).render(<App />);
+const body = document.body;
+const div: HTMLDivElement = document.createElement('div');
+
+if (body) {
+    document.body.appendChild(div);
+    ReactDOM.createRoot(div!).render(<App />);
+} else {
+    let observer: MutationObserver | null = new MutationObserver(() => {
+        const body = document.body;
+        if (body) {
+            if (observer) {
+                observer.disconnect();
+                observer = null;
+            }
+
+            document.body.appendChild(div);
+            ReactDOM.createRoot(div!).render(<App />);
+        }
+    });
+    observer.observe(document, { subtree: true, childList: true });
+}
